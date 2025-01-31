b56f46b4cce43b750d33de9b5414c1e94b91e688
asistan-emrah <asistanemrah@gmail.com>
2025-01-31 23:45:34 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,14 @@ rootProject.name = "CloudstreamPlugins"
 
 // This file sets what projects are included. Every time you add a new project, you must add it
 // to the includes below.
+// settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()  // AGP için Google deposu
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 
 // Plugins are included like this
 include(
