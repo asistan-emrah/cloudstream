ee8bb902a4be058206841b53357a8e914f0102ef
asistan-emrah <asistanemrah@gmail.com>
2025-01-31 21:29:45 +0300

    Create build.gradle.kts

---
+++ FilmekseniProvider/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    language = "tr"
+    description = "Filmekseni film ve dizi izleme sitesi için CloudStream eklentisi"
+    authors = listOf("CloudStream")
+
+    status = 1 // ok
+
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime",
+        "AnimeMovie"
+    )
+}
