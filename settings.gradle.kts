5405cebb047320373bfb5dfb301b1592d0bd3d36
asistan-emrah <asistanemrah@gmail.com>
2025-02-01 00:06:33 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,19 @@
-rootProject.name = "CloudstreamPlugins"
-
-// This file sets what projects are included. Every time you add a new project, you must add it
-// to the includes below.
 // settings.gradle.kts
 pluginManagement {
     repositories {
-        google()  // AGP için Google deposu
+        google()  // Android Gradle Plugin için
         mavenCentral()
         gradlePluginPortal()
     }
 }
 
-// Plugins are included like this
+rootProject.name = "CloudstreamPlugins"
+
+// Projeye dahil edilen modüller (tüm eklenti modüllerinizi buraya ekleyin)
 include(
-    "ExampleProvider"
+    "ExampleProvider", 
+    "FilmekseniProvider"
+    // Diğer modüllerinizi buraya ekleyin:
+    // "AnotherProvider",
+    // "MovieProvider"
 )
