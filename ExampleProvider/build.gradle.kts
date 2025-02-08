8f400b6781af27c621da27b4a42d11739b2303f7
asistan-emrah <asistanemrah@gmail.com>
2025-02-08 21:49:31 +0300

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,7 +1,44 @@
+plugins {
+    id("com.android.library") version "7.4.2"
+    id("org.jetbrains.kotlin.android") version "1.8.0"
+}
+
+android {
+    namespace = "com.example.lokal"
+    compileSdkVersion(34) // DÜZELTİLMİŞ SATIR
+
+    defaultConfig {
+        minSdk = 21
+        // buildToolsVersion KALDIRILDI
+    }
+
+    buildTypes {
+        named("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
 dependencies {
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    
+    // Cloudstream eklenti bağımlılıkları (gerekirse ekleyin)
+    // implementation("com.github.recloudstream:cloudstream:3.6.5")
 }
 // use an integer for version numbers
 version = -1
